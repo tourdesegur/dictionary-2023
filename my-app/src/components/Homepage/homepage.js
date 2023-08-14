cf6afd2121f7e457426932f330320c05e6f52fff
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2023-08-14 16:26:44 +0100

    upd icon

--- my-app/src/components/Homepage/homepage.js
+++ my-app/src/components/Homepage/homepage.js
@@ -1,6 +1,5 @@
 import './homepage.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
-import { faMagnifyingGlass } from '@fortawesome/free-regular-svg-icons'; // Import the specific icon you want to use
+
 const Homepage = () => {
   return (
     <div className="Homepage">
@@ -8,20 +7,16 @@ const Homepage = () => {
               <span className="title-d">:D</span>
               <span className="title">ictionary</span>
           </h1>
-          <div className="input-group mb-3">
-      <div className="input-group-prepend">
-        <span className="input-group-text bg-white">
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </span>
-      </div>
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Type a word"
-        aria-label="Type a word"
-        aria-describedby="basic-addon2"
-      />
-    </div>
+          <div className="box">
+            <img src='search-interface-symbol.png' alt='search-icon'/>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Type a word"
+              aria-label="Type a word"
+              aria-describedby="basic-addon2"
+            />
+          </div>
     </div>
   )
 }
